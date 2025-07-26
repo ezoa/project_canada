fc4ac7850b115d58835f7cc77dca44c9f738050e
kihero176 <zeibamarina@yahoo.fr>
2025-07-26 11:07:04 -0400

    mon peremier part 2 multiverse (#1)
    thank you

---
+++ main.py
@@ -0,0 +1,6 @@
+chercher_la_moyenne="entrez des note"
+francais=int(input("entrez un nombre de 1 a 10 POUR FRANCAIS"))
+anglais=int(input("entrez un nombre de 1 a 10 POUR ANGLAIS"))
+math=int(input("entre un nombre de 1 a 10 POUR MATH"))
+resultat=(francais+anglais+math)/3
+print("votre moyenne est ",resultat)
