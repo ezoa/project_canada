a335de521920e0475b3d67e7d214e17df5fceb5c
kihero176 <zeibamarina@yahoo.fr>
2025-08-02 09:50:59 -0400

    mon deuxieme commit

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-chercher_la_moyenne="entrez des note"
-francais=int(input("entrez un nombre de 1 a 10 POUR FRANCAIS"))
-anglais=int(input("entrez un nombre de 1 a 10 POUR ANGLAIS"))
-math=int(input("entre un nombre de 1 a 10 POUR MATH"))
-resultat=(francais+anglais+math)/3
-print("votre moyenne est ",resultat)
+# chercher_la_moyenne="entrez des note"
+# francais=int(input("entrez un nombre de 1 a 10 POUR FRANCAIS"))
+# anglais=int(input("entrez un nombre de 1 a 10 POUR ANGLAIS"))
+# math=int(input("entre un nombre de 1 a 10 POUR MATH"))
+# resultat=(francais+anglais+math)/3
+# print("votre moyenne est ",resultat)
+import tkinter as tk
+root=tk.Tk
+def moyenne():
+    francais=int(input("entrez les notes de 1 a 10 en francais"))
+    anglais =int(input("entrez les notes de 1 a 10 en anglais"))
+    math=int(input("entrez les notes de 1 a 10 en math"))
+    return francais,anglais,math
